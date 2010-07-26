0725d38047f2d9cd07617e037d9b532432dd041c
John Beranek <jberanek@users.sourceforge.net>
2010-07-26 11:12:49 +0000

    * Czech language update supplied by Marek Grac in SF #2800173

---
+++ convert_db_to_utf8.php
@@ -0,0 +1,150 @@
+<?php
+
+// $Id$
+
+// This script converts text in the database from a particular encoding
+// to UTF-8
+
+require_once "defaultincludes.inc";
+
+$encoding = get_form_var('encoding', 'string');
+
+header("Content-Type: text/html; charset=\"utf-8\"");
+
+?>
+
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <title>MRBS database encoding fixing script</title>
+  </head>
+  <body>
+
+  <h1>MRBS database encoding fixing script</h1>
+
+<?php
+
+
+if (!isset($encoding))
+{
+echo '
+
+    <p>
+      This script will convert all the text in the database from a selected encoding to
+      UTF-8, to use with MRBS\'s new Unicode feature ($unicode_encoding in config.inc.php
+      set to 1).<br>
+      <b>NOTE: Only run this script <u>once</u>. Running it more than once will
+      make a right mess of any non-ASCII text in the database. I\'d recommend you
+      backup your database before running this script if you\'re at all worried.</b>
+    </p>
+
+    <form method="get" action="'.$PHP_SELF.'">
+      Encoding to convert from:<br>
+      <select name="encoding">
+        <option value="iso-8859-1">Latin 1 (English/French/German/Italian/Norwegian etc.)</option>
+        <option value="iso-8859-2">Latin 2 (Czech)</option>
+        <option value="iso-8859-7">ISO-8859-7 (Greek)</option>
+        <option value="iso-8859-15">Latin 9 (European)</option>
+        <option value="Big-5">Big 5 (Chinese Traditional)</option>
+        <option value="Shift-JIS">Shift-JIS (Japanese)</option>
+      </select>
+      <br>
+      <input type="submit" value="Do it">
+    </form>
+';
+}
+else
+{
+  echo '
+    <p>
+      Starting update, this could take a while...
+    </p>
+
+    <p>
+      Updating areas:
+';
+
+  $sql = "SELECT id,area_name FROM mrbs_area";
+  $areas_res = sql_query($sql);
+
+  for ($i = 0; ($row = sql_row($areas_res, $i)); $i++)
+  {
+    $id = $row[0];
+    $name = addslashes(iconv($encoding,"utf-8",$row[1]));
+
+    $upd_sql = "UPDATE mrbs_area SET area_name='$name' WHERE id=$id";
+    sql_query($upd_sql);
+
+    echo ".";
+  }
+  echo " done.<br>Updating rooms: ";
+
+  $sql = "SELECT id,room_name,description,capacity FROM mrbs_room";
+  $rooms_res = sql_query($sql);
+
+  for ($i = 0; ($row = sql_row($rooms_res, $i)); $i++)
+  {
+    $id = $row[0];
+    $name = addslashes(iconv($encoding,"utf-8",$row[1]));
+    $desc = addslashes(iconv($encoding,"utf-8",$row[2]));
+    $capa = addslashes(iconv($encoding,"utf-8",$row[3]));
+
+    $upd_sql = "UPDATE mrbs_room SET room_name='$name',description='$desc',capacity='$capa' WHERE id=$id";
+    sql_command($upd_sql);
+
+    echo ".";
+  }
+  echo " done.<br>Updating repeating entries: ";
+
+  $sql = "SELECT id,name,description FROM mrbs_repeat";
+  $repeats_res = sql_query($sql);
+
+  for ($i = 0; ($row = sql_row($repeats_res, $i)); $i++)
+  {
+    $id = $row[0];
+    $name = addslashes(iconv($encoding,"utf-8",$row[1]));
+    $desc = addslashes(iconv($encoding,"utf-8",$row[2]));
+
+    $upd_sql = "UPDATE mrbs_repeat SET name='$name',description='$desc' WHERE id=$id";
+    sql_command($upd_sql);
+
+    echo ".";
+  }
+  echo " done.<br>Updating normal entries: ";
+
+  $sql = "SELECT id,name,description FROM mrbs_entry";
+  $entries_res = sql_query($sql);
+
+  for ($i = 0; ($row = sql_row($entries_res, $i)); $i++)
+  {
+    $id = $row[0];
+    $name = addslashes(iconv($encoding,"utf-8",$row[1]));
+    $desc = addslashes(iconv($encoding,"utf-8",$row[2]));
+
+    $upd_sql = "UPDATE mrbs_entry SET name='$name',description='$desc' WHERE id=$id";
+    sql_command($upd_sql);
+
+    echo ".";
+  }
+  echo '
+      done.
+    </p>
+
+    <p>
+      Finished everything that I can do.
+    </p>
+    
+    <p>
+      If you are using MySQL >= 4 you may still you need to modify
+      the database charset for textual columns. e.g.:
+    </p>
+    <pre>
+ALTER TABLE mrbs_area
+CHANGE area_name VARCHAR(30) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL
+</pre>
+';
+}
+?>
+  </body>
+</html>
+ 
---
+++ web/add.php
@@ -0,0 +1,95 @@
+<?php
+
+// $Id$
+
+require_once "defaultincludes.inc";
+
+// Get non-standard form variables
+$name = get_form_var('name', 'string');
+$description = get_form_var('description', 'string');
+$capacity = get_form_var('capacity', 'int');
+$type = get_form_var('type', 'string');
+
+// Check the user is authorised for this page
+checkAuthorised();
+
+// This file is for adding new areas/rooms
+
+// we need to do different things depending on if its a room
+// or an area
+
+if ($type == "area")
+{
+  // Truncate the name field to the maximum length as a precaution.
+  $name = substr($name, 0, $maxlength['area.area_name']);
+  $area_name_q = addslashes($name);
+  // Acquire a mutex to lock out others who might be editing the area
+  if (!sql_mutex_lock("$tbl_area"))
+  {
+    fatal_error(TRUE, get_vocab("failed_to_acquire"));
+  }
+  // Check that the area name is unique
+  if (sql_query1("SELECT COUNT(*) FROM $tbl_area WHERE area_name='$area_name_q' LIMIT 1") > 0)
+  {
+    $error = "invalid_area_name";
+  }
+  // If so, insert the area into the database
+  else
+  {
+    // Convert booleans to 1 or 0, as the fields are tinyints/smallints
+    $private_enabled = ($private_enabled) ? 1 : 0;
+    $private_default = ($private_default) ? 1 : 0;
+    $private_mandatory = ($private_mandatory) ? 1 : 0;
+    // Need to make sure the private settings are not NULL.   We can't specify a default
+    // in the SQL when creating the table because the default is a variable in the config file
+    $sql = "INSERT INTO $tbl_area 
+            (area_name, private_enabled, private_default, private_mandatory, private_override) 
+            VALUES ('$area_name_q', $private_enabled, $private_default, $private_mandatory, '$private_override')";
+    if (sql_command($sql) < 0)
+    {
+      fatal_error(1, sql_error());
+    }
+    $area = sql_insert_id("$tbl_area", "id");
+  }
+  // Release the mutex
+  sql_mutex_unlock("$tbl_area");
+}
+
+if ($type == "room")
+{
+  // Truncate the name and description fields to the maximum length as a precaution.
+  $name = substr($name, 0, $maxlength['room.room_name']);
+  $description = substr($description, 0, $maxlength['room.description']);
+  // Add SQL escaping
+  $room_name_q = addslashes($name);
+  $description_q = addslashes($description);
+  if (empty($capacity))
+  {
+    $capacity = 0;
+  }
+  // Acquire a mutex to lock out others who might be editing rooms
+  if (!sql_mutex_lock("$tbl_room"))
+  {
+    fatal_error(TRUE, get_vocab("failed_to_acquire"));
+  }
+  // Check that the room name is unique within the area
+  if (sql_query1("SELECT COUNT(*) FROM $tbl_room WHERE room_name='$room_name_q' AND area_id=$area LIMIT 1") > 0)
+  {
+    $error = "invalid_room_name";
+  }
+  // If so, insert the room into the datrabase
+  else
+  {
+    $sql = "INSERT INTO $tbl_room (room_name, sort_key, area_id, description, capacity)
+            VALUES ('$room_name_q', '$room_name_q', $area, '$description_q',$capacity)";
+    if (sql_command($sql) < 0)
+    {
+      fatal_error(1, sql_error());
+    }
+  }
+  // Release the mutex
+  sql_mutex_unlock("$tbl_room");
+}
+
+$returl = "admin.php?area=$area" . (!empty($error) ? "&error=$error" : "");
+header("Location: $returl");
